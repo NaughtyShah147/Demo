946db58925666fee5f8e1d73c32c2cdc31689d78
NaughtyShah147 <zeeshan.shah147@gmail.com>
2021-06-24 07:53:22 +0500

    Patch work

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using API.Extensions;
-
+using API.Middleware;
 namespace API
 {
     public class Startup
@@ -41,10 +41,10 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
+            app.UseMiddleware<ExceptionMiddleware>();
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
                 
             }
 
